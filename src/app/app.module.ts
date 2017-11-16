bbeeeeeffa3c00cb01a64666acd057fbc8cc6fc2
Kahou <creator.kahou@gmail.com>
2017-11-16 13:57:32 +0800

    new project

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    NgZorroAntdModule.forRoot()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
