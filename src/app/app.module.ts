26fd4a7f23ac1847b4e522b6d65684ecb24c1b01
Kahou <creator.kahou@gmail.com>
2017-12-24 18:22:06 +0800

    ng g c goods

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from './modules/material.module';
 import { AppComponent } from './app.component';
+import { GoodsComponent } from './goods/goods.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoodsComponent
   ],
   imports: [
     BrowserModule,
