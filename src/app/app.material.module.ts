c027348e183dcd9e28b2bef52670198bbe951760
Kahou <creator.kahou@gmail.com>
2017-12-24 18:07:31 +0800

    basic layout

---
+++ src/app/app.material.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/Sidenav';
+import { MatButtonModule } from '@angular/material/Button';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+@NgModule({
+  exports:[
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule
+  ]
+})
+export class AppMaterialModule { }
